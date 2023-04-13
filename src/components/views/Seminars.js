640fcd6616702c9d1cf1587e0bbcf9a3025415aa
Filip Troníček <filip@gitpod.io>
2023-04-13 21:22:23 +0000

    Semi

--- src/components/views/Seminars.js
+++ src/components/views/Seminars.js
@@ -12,7 +12,7 @@ export default function SeminarsList() {
 
             let { data: seminars, error } = await supabase
                 .from('seminars')
-                .select('*')
+                .select('*');
 
             setSeminars(seminars);
         })()
