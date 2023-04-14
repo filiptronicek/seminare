1cf94d092d78ec3597452b5511074fb3e796d177
Filip Troníček <filip@gitpod.io>
2023-04-14 07:48:57 +0000

    Handle error logic

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -1,12 +1,28 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Auth } from '@supabase/auth-ui-react'
+import {useSearchParams} from 'next/navigation';
 import supabase from 'src/lib/supabase-browser';
 
 
 const SignIn = () => {
   const [errorMsg, setErrorMsg] = useState(null);
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (searchParams.has("error")) {
+      const errorMessage = searchParams.get("error_description");
+      switch (errorMessage) {
+        case "Error creating identity":
+          setErrorMsg("Pro přihlášení musíte použít školní e-mail")
+          break;
+        default:
+          setErrorMsg(errorMessage);
+      }
+
+    }
+  }, [searchParams]);
 
   return (
     <div className="card">
