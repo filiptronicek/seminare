49cd9e36790b1a3b5f060a4507789aff35259931
Filip Troníček <filip@gitpod.io>
2023-10-21 20:38:24 +0000

    Simple detail scaffold

--- src/pages/events/[...id].tsx
+++ src/pages/events/[...id].tsx
@@ -1,6 +1,25 @@
-import { useRouter } from 'next/router'
- 
+import { useRouter } from "next/router";
+import { api } from "~/utils/api";
+
+const normalizeId = (id: string | string[]) => {
+    if (Array.isArray(id)) {
+        return id[0]!;
+    }
+    return id;
+}
+
 export default function Page() {
-  const router = useRouter()
-  return <p>Akce: {router.query.id}</p>
+    const router = useRouter();
+    const { data: event, error } = api.example.getEvent.useQuery({ id: normalizeId(router.query.id!) });
+    return (
+        <>
+            {error && <div>failed to load</div>}
+            {event && (
+                <div>
+                    <h1>{event.title}</h1>
+                    <p>{event.description}</p>
+                </div>
+            )}
+        </>
+    );
 }
--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -22,6 +22,9 @@ export const exampleRouter = createTRPCRouter({
         return ctx.db.student.findMany();
     }),
     listEvents : publicProcedure.query(({ ctx }) => {
-        return ctx.db.event.findMany();
+        return ctx.db.event.findMany({take: 10});
+    }),
+    getEvent : publicProcedure.input(z.object({id: z.string()})).query(({input, ctx}) => {
+        return ctx.db.event.findUnique({where: {id: input.id}});
     }),
 });
