e7d63b55b9db57d36c0909a208186790ce4972f9
Filip Troníček <filip@gitpod.io>
2023-11-12 19:33:50 +0000

    Add skeleton loading for student data in Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 
 import { SingleEventCard } from "@/components/ui/SingleEventCard";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "~/utils/api";
 
 export default function Home() {
     const events = api.events.listEvents.useQuery({ active: true });
-    const { data: studentData } = api.events.getStudent.useQuery();
+    const { data, isLoading } = api.events.getStudent.useQuery();
 
     return (
         <section className="flex min-h-screen flex-col items-start">
-            <h1 className="text-4xl font-bold">
-                Nadcházející akce pro <u>{studentData?.class}</u>
-            </h1>
+            {isLoading ? (
+                <Skeleton className="w-[500px] h-[40px] rounded-full" />
+            ) : (
+                <h1 className="text-4xl font-bold">
+                    Nadcházející akce pro <u>{data?.class}</u>
+                </h1>
+            )}
             <div className="mt-8 flex flex-row flex-wrap gap-3 justify-around md:justify-start">
                 {events.data?.map((event) => <SingleEventCard key={event.id} event={event} />)}
             </div>
