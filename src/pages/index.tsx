cd785530c49cefbf4a3f2e8e3460181d7a053705
Filip Troníček <filip@gitpod.io>
2024-02-11 23:38:15 +0000

    Modal to force class input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,48 @@
+"use client";
+
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 
 import { SingleEventCard } from "@/components/ui/SingleEventCard";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "~/utils/api";
+import { ClassForm } from "./settings";
+import { Loader2 } from "lucide-react";
 
 export default function Home() {
     const events = api.events.listEvents.useQuery({ active: true });
-    const { data, isLoading } = api.events.getStudent.useQuery();
+    const { data: user, isLoading, isError } = api.events.getStudent.useQuery();
+
+    if (!isLoading && !user?.class) {
+        return (
+            <Dialog open={true}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Dokonči nastavení</DialogTitle>
+                        <DialogDescription className="flex flex-col gap-4">
+                            <span>Nastav si prosím svoji třídu, abychom ti mohli ukázat akce, které se tě týkají.</span>
+                            <ClassForm />
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+        );
+    }
+
+    if (isLoading) {
+        return <Loader2 className="animate-spin" />;
+    }
+
+    if (isError || !user) {
+        return "Naskytla se chyba v načítání uživatelských dat";
+    }
 
     return (
         <section className="flex min-h-screen flex-col items-start">
             {isLoading ?
                 <Skeleton className="w-[500px] h-[40px] rounded-full" />
             :   <h1 className="text-4xl font-bold">
-                    Nadcházející akce pro <u>{data?.class}</u>
+                    Nadcházející akce pro <u>{user.class}</u>
                 </h1>
             }
             <div className="mt-8 flex flex-row flex-wrap gap-3 justify-around md:justify-start">
--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -13,6 +13,7 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { ACCESSIBLE_CLASSES } from "~/utils/constants";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { api } from "~/utils/api";
+import { Loader2 } from "lucide-react";
 
 const FormSchema = z.object({
     currentClass: z.string({
@@ -20,23 +21,86 @@ const FormSchema = z.object({
     }),
 });
 
-export default function Settings() {
-    const { data: userData } = useUser();
-    const { data: student, isLoading: isStudentLoading, isError: isStudentError } = api.events.getStudent.useQuery();
+export const ClassForm = () => {
     const updateMutation = api.userSettings.changeStudentClass.useMutation();
+    const {
+        data: student,
+        isLoading: isStudentLoading,
+        isError: isStudentError,
+        refetch,
+    } = api.events.getStudent.useQuery();
+    const utils = api.useContext();
 
     const form = useForm<z.infer<typeof FormSchema>>({
         resolver: zodResolver(FormSchema),
     });
 
     const onSubmit = (data: z.infer<typeof FormSchema>) => {
-        updateMutation.mutate({ class: data.currentClass });
-        toast({
-            title: "Třída byla úspěšně změněna",
-        });
+        updateMutation.mutate(
+            { class: data.currentClass },
+            {
+                onSuccess: () => {
+                    void utils.events.getStudent.invalidate();
+
+                    toast({
+                        title: "Třída byla úspěšně změněna",
+                    });
+
+                    void refetch();
+                },
+            },
+        );
     };
 
-    if (!userData) {
+    if (isStudentLoading) {
+        return <Loader2 className="animate-spin" />;
+    }
+
+    if (isStudentError) {
+        return "Nastal problém s načítáním uživatelských informací.";
+    }
+
+    return (
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="grid w-full max-w-sm items-center gap-1.5 space-y-6"
+            >
+                <FormField
+                    control={form.control}
+                    name="currentClass"
+                    defaultValue={student?.class ?? undefined}
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Třída</FormLabel>
+                            <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                <FormControl>
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="..." />
+                                    </SelectTrigger>
+                                </FormControl>
+                                <SelectContent>
+                                    {[...ACCESSIBLE_CLASSES].map((item) => (
+                                        <SelectItem key={item} value={item}>
+                                            {item}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                            <FormDescription>Vyber si svou třídu.</FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Uložit</Button>
+            </form>
+        </Form>
+    );
+};
+
+export default function Settings() {
+    const { data: user } = useUser();
+    if (!user) {
         return null;
     }
 
@@ -50,58 +114,16 @@ export default function Settings() {
                         type="name"
                         id="name"
                         placeholder="Tvoje jméno"
-                        value={getUserName(userData)}
+                        value={getUserName(user)}
                         disabled
                         readOnly
                     />
                 </div>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
                     <Label htmlFor="email">E-mail</Label>
-                    <Input
-                        type="email"
-                        id="email"
-                        placeholder="Tvůj e-mail"
-                        value={userData?.email}
-                        disabled
-                        readOnly
-                    />
+                    <Input type="email" id="email" placeholder="Tvůj e-mail" value={user?.email} disabled readOnly />
                 </div>
-                {!isStudentLoading && !isStudentError && (
-                    <Form {...form}>
-                        <form
-                            onSubmit={form.handleSubmit(onSubmit)}
-                            className="grid w-full max-w-sm items-center gap-1.5 space-y-6"
-                        >
-                            <FormField
-                                control={form.control}
-                                name="currentClass"
-                                defaultValue={student?.class ?? [...ACCESSIBLE_CLASSES][0]}
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel>Třída</FormLabel>
-                                        <Select onValueChange={field.onChange} defaultValue={field.value}>
-                                            <FormControl>
-                                                <SelectTrigger>
-                                                    <SelectValue placeholder="..." />
-                                                </SelectTrigger>
-                                            </FormControl>
-                                            <SelectContent>
-                                                {[...ACCESSIBLE_CLASSES].map((item) => (
-                                                    <SelectItem key={item} value={item}>
-                                                        {item}
-                                                    </SelectItem>
-                                                ))}
-                                            </SelectContent>
-                                        </Select>
-                                        <FormDescription>Vyber si svou třídu.</FormDescription>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-                            <Button type="submit">Uložit</Button>
-                        </form>
-                    </Form>
-                )}
+                <ClassForm />
             </div>
         </section>
     );
