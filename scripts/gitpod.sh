ddaadcec704cbdd1749c3c4bf143507b408cea6a
Filip Troníčeka <filip@gitpod.io>
2024-02-25 09:48:20 +0000

    simplify startup script

--- scripts/gitpod.sh
+++ scripts/gitpod.sh
@@ -3,3 +3,6 @@
 docker pull postgres
 docker run --name pgdb -e POSTGRES_PASSWORD=development -p 5432:5432 -d postgres
 docker exec -it pgdb psql -U postgres -c "CREATE DATABASE interclip;"
+
+bun run db:push
+bun run prisma/seed.ts
