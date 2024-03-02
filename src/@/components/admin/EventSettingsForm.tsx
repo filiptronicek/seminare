5b584294377e84230f0ba32f59a3567759a1b2c9
Filip Troníčeka <filip@gitpod.io>
2024-03-02 17:22:35 +0000

    Simple edit form

---
+++ src/@/components/admin/EventSettingsForm.tsx
@@ -0,0 +1,84 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useCallback } from "react";
+
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { type Event } from "@prisma/client";
+import { Checkbox } from "../ui/checkbox";
+
+const formSchema = z.object({
+    title: z.string().min(2).max(50),
+    description: z.string().max(2500).optional(),
+    multipleOptionsPerUser: z.boolean(),
+});
+
+type Props = {
+    event: Event;
+};
+export const EventSettingsForm = ({ event }: Props) => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: event.title,
+            description: event.description ?? "",
+            multipleOptionsPerUser: event.allowMultipleSelections,
+        },
+    });
+
+    const onSubmit = useCallback((values: z.infer<typeof formSchema>) => {
+        console.log(values);
+    }, []);
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Název</FormLabel>
+                            <FormControl>
+                                <Input placeholder="" {...field} />
+                            </FormControl>
+                            <FormDescription>Název akce, který bude zobrazen na seznamu akcí.</FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Popis</FormLabel>
+                            <FormControl>
+                                <Input placeholder="" {...field} />
+                            </FormControl>
+                            <FormDescription>Popis akce, který bude zobrazen na detailu akce.</FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="multipleOptionsPerUser"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <Checkbox className="mr-2" {...field} />
+                            </FormControl>
+                            <FormLabel>Více možností na uživatele</FormLabel>
+                            <FormDescription>Umožnit uživatelům zvolit více možností této jedné akce.</FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Uložit</Button>
+            </form>
+        </Form>
+    );
+};
--- src/pages/admin/[eventId].tsx
+++ src/pages/admin/[eventId].tsx
@@ -3,18 +3,21 @@ import { useRouter } from "next/router";
 import { EventOptionTable } from "@/components/admin/EventOptionTable";
 import { api } from "~/utils/api";
 
-export default function Home() {
+import { EventSettingsForm } from "@/components/admin/EventSettingsForm";
+
+export default function EventOption() {
     const { query } = useRouter();
     const eventId = query.eventId as string;
 
     const { data: event } = api.singleEvent.getEvent.useQuery({ id: eventId });
+
     if (!eventId || !event) {
         return <Loader2 className="animate-spin" />;
     }
 
     return (
         <section className="flex min-h-screen flex-col items-start gap-8">
-            <h1 className="text-3xl font-bold">Akce {`"${event.title}"`}</h1>
+            <EventSettingsForm event={event} />
             <EventOptionTable id={eventId} />
         </section>
     );
