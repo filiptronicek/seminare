d85dee8a6975c5859787b91ccbf64b3269bf9014
Filip Troníček <filip@gitpod.io>
2024-04-02 12:13:35 +0000

    Allow universal signups

--- src/server/api/routers/event-options.ts
+++ src/server/api/routers/event-options.ts
@@ -83,7 +83,7 @@ export const eventOptionsRouter = createTRPCRouter({
             }
         }
 
-        if (option.event.visibleToClasses && !option.event.visibleToClasses.includes(user.class)) {
+        if (option.event.visibleToClasses && option.event.visibleToClasses.length >= 0 && !option.event.visibleToClasses.includes(user.class)) {
             throw new TRPCError({
                 code: "PRECONDITION_FAILED",
                 message: "This event is not available to your class",
