3f4e638be59fdf29c0c21bc38a1947103d5ceae2
Filip Troníček <filip@gitpod.io>
2024-04-20 08:36:01 +0000

    fix gitpod maybe?

--- scripts/gitpod.sh
+++ scripts/gitpod.sh
@@ -4,5 +4,7 @@ docker pull postgres
 docker run --name pgdb -e POSTGRES_PASSWORD=development -p 5432:5432 -d postgres
 docker exec -it pgdb psql -U postgres -c "CREATE DATABASE interclip;"
 
+gp ports await 5432
+
 bun run db:push
 bun run prisma/seed.ts
