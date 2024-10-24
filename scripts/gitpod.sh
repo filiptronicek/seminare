4c95a1a2398c27ce4c89fdd3334ddcef34c20ef2
Filip Troníček <filip@gitpod.io>
2024-10-24 17:13:07 +0000

    fix DB name

--- scripts/gitpod.sh
+++ scripts/gitpod.sh
@@ -2,7 +2,7 @@
 
 docker pull postgres
 docker run --name pgdb -e POSTGRES_PASSWORD=development -p 5432:5432 -d postgres
-docker exec -it pgdb psql -U postgres -c "CREATE DATABASE interclip;"
+docker exec -it pgdb psql -U postgres -c "CREATE DATABASE seminare;"
 
 gp ports await 5432
 
